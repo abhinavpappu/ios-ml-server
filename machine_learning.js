dcb0bbfe24293f8cef3679c0258ba99e92ef3a66
Abhinav Pappu <abhi1pappu9@gmail.com>
2019-05-13 01:03:02 +0000

    Finished training and prediction functions using facenet

--- machine_learning.js
+++ machine_learning.js
@@ -1,20 +1,75 @@
-const tf = require("@tensorflow/tfjs-node");
-const knnClassifier = require("@tensorflow-models/knn-classifier");
-const mobilenet = require("@tensorflow-models/mobilenet");
 const { loadImage, createCanvas, Image } = require('canvas');
 const fs = require("fs");
 const { promisify } = require("util");
+const { Facenet } = require("facenet");
+
+const facenet = new Facenet();
 
 const readdir = promisify(fs.readdir);
 const writeFile = promisify(fs.writeFile);
 const readFile = promisify(fs.readFile);
 
+let time = Date.now();
+function log(message) {
+    console.log(message + ` in ${Date.now() - time}ms`);
+    time = Date.now();
+}
+
 async function train(images, isBase64) {
+    log('Started training');
     
+    const vectors = [];
+    for (const image of images) {
+        // find the vector associated with the face in the image
+        const vector = await imageToVector(image, isBase64);
+        vectors.push(vector);
+    }
+    
+    log('Converted images to vectors');
+    
+    // Find the average of all the vectors
+    const averageVector = Array(vectors[0].length).fill(0);
+    for (const vector of vectors) {
+        averageVector = addVectors(averageVector, vector);
+    }
+    for (const [index, value] of averageVector.entries()) {
+        averageVector[index] = value / vectors.length;
+    }
+    
+    // save the model
+    const jsonData = JSON.stringify({
+        vector: averageVector,
+        numImages: images.length,
+    });
+    const existingModels = await readdir('./models');
+    const filename = getRandomFilename('.json', existingModels);
+    await writeFile(`./models/${filename}`, jsonData);
+    
+    log('Saved model');
+    
+    return filename;
 }
 
 async function predict(model, image, isBase64) { // model is a filename pointing to the saved model
+    // Load the model
+    const data = JSON.parse(await readFile(`./models/${model}`));
+    
+    const vector = await imageToVector(image, isBase64);
+    
+    return distanceBetween(data.vector, vector);
+}
 
+async function imageToVector(image, isBase64) {
+    // need to convert image string to type ImageData
+    const actualImage = await createImage(image, isBase64);
+    
+    // find face from image (we assume that there is only one, and get the first face found)
+    const face = await facenet.align(actualImage);
+    
+    // find the vector associated with the face
+    const vector = await facenet.embedding(face);
+    
+    return vector
 }
 
 function createImage(src, isBase64) {
@@ -24,11 +79,30 @@ function createImage(src, isBase64) {
             const canvas = createCanvas(image.width, image.height);
             const ctx = canvas.getContext('2d');
             ctx.drawImage(image, 0, 0);
-            resolve(canvas);
+            resolve(ctx.getImageData(0, 0, image.width, image.height));
         }
         image.onerror = () => reject('Error loading image');
         image.src = (isBase64 ? 'data:image/png;base64,' : '') + src;
     });
+}
+
+function addVectors(v1, v2, multiplier = 1) {
+    // we assume that they are the same size
+    const v1Copy = v1.slice(0);
+    for (const [index, value] of v2.entries()) {
+        v1Copy[index] += multiplier * value;
+    }
+    return v1Copy;
+}
+
+const subtractVectors = (v1, v2) => addVectors(v1, v2, -1);
+
+function distanceBetween(v1, v2) {
+    const difference = subtractVectors(v1, v2);
+    const sumOfSquares = difference
+        .map(x => Math.pow(x, 2))
+        .reduce((total, value) => total + value, 0);
+    return Math.sqrt(sumOfSquares);
 }
 
 function getRandomFilename(extension = '', existingFilenames = []) {
@@ -47,37 +121,5 @@ function getRandomString(length) {
     }
     return str.substr(0, length);
 }
-
-// Modified from https://github.com/tensorflow/tfjs/issues/633#issuecomment-456308218
-async function save(classifier, location) {
-   let dataset = classifier.getClassifierDataset()
-   var datasetObj = {}
-   
-   Object.keys(dataset).forEach((key) => {
-     let data = dataset[key].dataSync();
-     // use Array.from() so when JSON.stringify() it covert to an array string e.g [0.1,-0.2...] 
-     // instead of object e.g {0:"0.1", 1:"-0.2"...}
-     datasetObj[key] = Array.from(data);
-   });
-   
-   let jsonStr = JSON.stringify(datasetObj);
-   await writeFile(location, jsonStr);
- }
- 
-// Modified from https://github.com/tensorflow/tfjs/issues/633#issuecomment-456308218
-async function load(location) {
-    const classifier = knnClassifier.create();
-    const dataset = await readFile(location);
-    
-    let tensorObj = JSON.parse(dataset)
-    //covert back to tensor
-    Object.keys(tensorObj).forEach((key) => {
-      tensorObj[key] = tf.tensor(tensorObj[key], [tensorObj[key].length / 1024, 1024])
-    });
-    
-    classifier.setClassifierDataset(tensorObj);
-    return classifier;
-}
-
 
 module.exports = { train, predict };
