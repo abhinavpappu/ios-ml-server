c05c4153671e69907d65e5afb2722f890134dc67
Abhinav Pappu <abhi1pappu9@gmail.com>
2019-05-13 16:57:47 -0500

    Finished server

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
-const tf = require("@tensorflow/tfjs-node");
-const mobilenet = require("@tensorflow-models/mobilenet");
-const canvas = require("canvas");
 const express = require("express");
+const { train, predict } = require('./machine_learning.js');
 
 const app = express();
 const port = 8080;
-const host = '0.0.0.0';
+// const host = '0.0.0.0';
+const host = '127.0.0.1';
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
+})
+
+app.post('/train', (req, res) => {
+    const images = JSON.parse(req.query.images);
+    train(images, true).then(modelName => res.send(modelName));
+});
+
+app.post('/predict', (req, res) => {
+    const image = req.query.image;
+    predict(image, true).then(distance => res.send(distance));
 })
 
 app.listen(port, host, () => {
